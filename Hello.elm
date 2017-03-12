95313a9fc810dfbbd0232da1ef5a44b97cd0a0a1
Anna Konopka <ania@zdwola.pl>
2017-03-12 19:50:18 +0100

    A few more divs + styles

--- Hello.elm
+++ Hello.elm
@@ -1,7 +1,17 @@
 module Hello exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, div, node)
+import Html.Attributes exposing (class, rel, href)
 
+css path =
+  node "link" [ rel "stylesheet", href path ] []
 
-main =
-    text "Hello"
+main = 
+    div [] [
+        css "styles.css",
+        div [class "hello"] [
+            div [] [text "My timer"],
+            div [] [text "25:00"],
+            div [class "button"] [text "Start!"]
+        ]
+    ]
